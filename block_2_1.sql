6eecec7f7aea881bf4f4f7c9e347b584513d7884
Anton <antosha250900@gmail.com>
2019-12-28 20:15:36 +0300

    Fixed block2_1

--- block_2_1.sql
+++ block_2_1.sql
@@ -1,8 +1,10 @@
 -- Выберете информацию об аптеках и объемах их продаж (в рублях), отсортировав от больших объемов к меньшим.
 
-SELECT pharmacy.address, pharmacy.code, SUM(sell_medicine.count * medicine.price) as total_price FROM pharmacy 
-LEFT JOIN sell_medicine ON pharmacy.id = sell_medicine.fk_pharmacy
-JOIN medicine ON sell_medicine.fk_medicine = medicine.id
-GROUP BY pharmacy.id
+SELECT pharmacy.address, pharmacy.code, SUM(IF(sell_medicine.count, sell_medicine.count * medicine.price, 0)) as total_price 
+FROM pharmacy 
+LEFT JOIN sell_medicine ON pharmacy.id = sell_medicine.fk_pharmacy 
+LEFT JOIN medicine ON sell_medicine.fk_medicine = medicine.id 
+GROUP BY pharmacy.id 
 ORDER BY total_price DESC;
+
 
